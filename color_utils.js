28ab48ef1fcea0d818a9fe3f871381d0ce8f9b52
kevinelong <kevinelong@gmail.com>
2023-06-06 20:58:11 -0700

    updates

--- color_utils.js
+++ color_utils.js
@@ -3,7 +3,7 @@
     // const perLine = 100 / 37;
     function getPallette(){
         return COLORS.map((color) => {
-        return `<div class="swatch" style="background:${color.name}"></div>`;
+        return `<div class="swatch" style="background:${color.name}" onclick="selectedColor=this.style.backgroundColor"></div>`;
     }).join('');
 }
 
